4be809f2e350b50d5925dab2deeea743433b0cbd
Samantha Jordan <shinyguns@gmail.com>
2020-01-22 19:23:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-if phase.downcase == phase
+if phase == phase.downcase
   return "Huh?! SPEAK UP, SONNY!"
 
 # If you shout, she can hear you (or at least she thinks so) 
