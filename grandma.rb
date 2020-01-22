9b996c227538d0fb278cc0489aed3596209f9a9e
Samantha Jordan <shinyguns@gmail.com>
2020-01-22 19:57:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,12 @@
 def speak_to_grandma(phase)
 
-  if phase == phase.capitalize || phase.lowercase
-    return "HUH?! SPEAK UP SONNY!"
+  if phase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
 
   elsif phase == phase.upcase && (phase.upcase != "I LOVE YOU GRANDMA!")
     return "NO, NOT SINCE 1938!"
 
-  else phase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  else phase
+    return "HUH? SPEAK UP, SONNY!"
   end
 end
