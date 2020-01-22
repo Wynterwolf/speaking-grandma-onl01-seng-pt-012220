5c83aa53b39260b0a6da4bd2c3061e3280e532ec
Samantha Jordan <shinyguns@gmail.com>
2020-01-22 19:15:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,15 +5,15 @@ def speak_to_grandma(phrase)
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-if phase.capitalize == false
-  return "HUH?! SPEAK UP, SONNY!"
+if phrase.upcase == false
+  return "Huh?! SPEAK UP, SONNY!"
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
 # NO, NOT SINCE 1938!
 
-elsif phase.upcase == true
+elsif phrase.upcase == true
   return "NO, NOT SINCE 1938!"
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
