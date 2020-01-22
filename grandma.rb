1d81ee984dd84216cee7455961ae2d97f6322d6d
Samantha Jordan <shinyguns@gmail.com>
2020-01-22 19:08:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-if phrase.match(/\p{Lower}/) == true
+if phrase.match(/\p{Lower}/) == false
   return "Huh?! SPEAK UP, SONNY!"
 
 # If you shout, she can hear you (or at least she thinks so) 
@@ -13,7 +13,7 @@ if phrase.match(/\p{Lower}/) == true
 
 # NO, NOT SINCE 1938!
 
-elsif phrase.match(/\p{Lower}/) == false
+elsif phrase.match(/\p{Lower}/) == true
   return "NO, NOT SINCE 1938!"
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
